e609d1a34550b764f4737c7ce30a04b58f47f5da
Ramiz Ali <ramiz.apk7@gmail.com>
2023-03-18 05:28:42 +0300

    complated assignment

--- index.js
+++ index.js
@@ -8,6 +8,79 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+// Your code here
+function createEmployeeRecord(params) {
+    let myObj = {
+        firstName: params[0],
+        familyName: params[1],
+        title: params[2],
+        payPerHour: params[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+    return myObj;
+
+}
+
+function createEmployeeRecords(array) {
+    let newArray = [];
+
+    for (const employee of array) {
+
+        newArray.push(createEmployeeRecord(employee));
+
+    }
+    return newArray;
+}
+
+const createTimeInEvent = function (dataStamp) {
+    let [date, hour] = dataStamp.split(' ');
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    });
+
+    return this;
+}
+
+let createTimeOutEvent = function (dataStamp) {
+    let [date, hour] = dataStamp.split(' ');
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this;
+}
+
+function hoursWorkedOnDate(dataStamp) {
+    const timeIn = this.timeInEvents.find(function (e) {
+        return e.date === dataStamp
+    });
+    const timeOut = this.timeOutEvents.find(function (e) {
+        return e.date === dataStamp
+    });
+
+    let workedHours = (timeOut.hour - timeIn.hour) / 100;
+    // console.log(workedHours);
+    return workedHours;
+}
+
+function wagesEarnedOnDate(dataStamp) {
+    let workedHours = hoursWorkedOnDate.call(this, dataStamp);
+    let payPerHour = this.payPerHour;
+    let EarnedOnDate = workedHours * payPerHour;
+    return EarnedOnDate;
+}
+let findEmployeeByFirstName = function (array, name) {
+    return array.find(function (arr) {
+        return arr.firstName === name;
+    })
+}
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
@@ -21,3 +94,14 @@ const allWagesFor = function () {
     return payable
 }
 
+
+
+
+
+function calculatePayroll(recordObj) {
+    let all = recordObj.reduce(function (acc, value) {
+        return acc + allWagesFor.call(value)
+    }, 0);
+
+    return all;
+}
